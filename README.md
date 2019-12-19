deff253e054468e2eab3545609344f636e0d504b
Felipe Diniz <diniz.ime@gmail.com>
2019-12-19 06:33:37 -0300

    load

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-﻿# Serviço de Autenticação v0.1
+﻿# Serviço de Autenticação v1.0
 
 Serviço em Node.js para autenticação de usuários. Sua utilização é necessária para executar os seguintes serviços da DSG:
 
-* Sistema de Apoio a Produção (SAP)
-* DSGDocs
-* Gerenciador do FME
-* Sistema de Pedidos Mapoteca
-* Sistema de Controle do Acervo
+- Sistema de Apoio a Produção (SAP)
+- DSGDocs
+- Gerenciador do FME
+- Sistema de Pedidos Mapoteca
+- Sistema de Controle do Acervo
--- auth-client/src/components/Cadastro/index.js
+++ auth-client/src/components/Cadastro/index.js
@@ -4,6 +4,7 @@ import { Link, withRouter } from "react-router-dom";
 import { Form, Container } from "./styles";
 import api from "../../services/api";
 import { useAlert } from "react-alert";
+import ReactLoading from "react-loading";
 
 const Cadastro = withRouter(props => {
   const [usuario, setUsuario] = useState("");
@@ -16,6 +17,7 @@ const Cadastro = withRouter(props => {
   const [listaTurnos, setListaTurnos] = useState([]);
   const [listaPostoGrad, setListaPostoGrad] = useState([]);
   const [error, setError] = useState("");
+  const [loaded, setLoaded] = useState(false);
 
   const alert = useAlert();
 
@@ -37,10 +39,6 @@ const Cadastro = withRouter(props => {
         setError("Erro com o serviço de autenticação.");
       }
     }
-    getTipoPostoGrad();
-  }, []);
-
-  useEffect(() => {
     async function getTurnos() {
       try {
         const response = await api.get("/usuarios/tipo_turno");
@@ -58,7 +56,10 @@ const Cadastro = withRouter(props => {
         setError("Erro com o serviço de autenticação.");
       }
     }
-    getTurnos();
+
+    Promise.all([getTipoPostoGrad(), getTurnos()]).then(() => {
+      setLoaded(true);
+    });
   }, []);
 
   const handleSignUp = async e => {
@@ -113,63 +114,67 @@ const Cadastro = withRouter(props => {
 
   return (
     <Container>
-      <Form onSubmit={handleSignUp}>
-        <h1>Cadastrar novo usuário</h1>
-        <br />
-        {error && <p>{error}</p>}
-        <input
-          type="text"
-          placeholder="Nome de usuário"
-          onChange={e => setUsuario(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Senha"
-          onChange={e => setSenha(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Confirme a senha"
-          onChange={e => setConfirmarSenha(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Nome completo"
-          onChange={e => setNome(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Nome de guerra"
-          onChange={e => setNomeGuerra(e.target.value)}
-        />
-        <select defaultValue="" onChange={handlePostoGrad}>
-          <option value="" disabled>
-            Selecione seu Posto/Graduação
-          </option>
-          {listaPostoGrad.map(function(n) {
-            return (
-              <option key={n.code} value={n.code}>
-                {n.nome_abrev}
-              </option>
-            );
-          })}
-        </select>
-        <select defaultValue="" onChange={handleTurno}>
-          <option value="" disabled>
-            Selecione seu turno de trabalho
-          </option>
-          {listaTurnos.map(function(n) {
-            return (
-              <option key={n.code} value={n.code}>
-                {n.nome}
-              </option>
-            );
-          })}
-        </select>
-        <button type="submit">Cadastrar</button>
-        <hr />
-        <Link to="/">Fazer login</Link>
-      </Form>
+      {loaded ? (
+        <Form onSubmit={handleSignUp}>
+          <h1>Cadastrar novo usuário</h1>
+          <br />
+          {error && <p>{error}</p>}
+          <input
+            type="text"
+            placeholder="Nome de usuário"
+            onChange={e => setUsuario(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Senha"
+            onChange={e => setSenha(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Confirme a senha"
+            onChange={e => setConfirmarSenha(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Nome completo"
+            onChange={e => setNome(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Nome de guerra"
+            onChange={e => setNomeGuerra(e.target.value)}
+          />
+          <select defaultValue="" onChange={handlePostoGrad}>
+            <option value="" disabled>
+              Selecione seu Posto/Graduação
+            </option>
+            {listaPostoGrad.map(function(n) {
+              return (
+                <option key={n.code} value={n.code}>
+                  {n.nome_abrev}
+                </option>
+              );
+            })}
+          </select>
+          <select defaultValue="" onChange={handleTurno}>
+            <option value="" disabled>
+              Selecione seu turno de trabalho
+            </option>
+            {listaTurnos.map(function(n) {
+              return (
+                <option key={n.code} value={n.code}>
+                  {n.nome}
+                </option>
+              );
+            })}
+          </select>
+          <button type="submit">Cadastrar</button>
+          <hr />
+          <Link to="/">Fazer login</Link>
+        </Form>
+      ) : (
+        <ReactLoading type="bars" color="#fff" height={"7%"} width={"7%"} />
+      )}
     </Container>
   );
 });
