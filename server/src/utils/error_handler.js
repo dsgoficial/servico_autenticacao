145c0e471e9dd046d2e479c29ab790b0f8b669fa
Felipe Diniz <diniz.ime@gmail.com>
2019-12-30 05:50:11 -0300

    fix error handler

--- server/src/utils/error_handler.js
+++ server/src/utils/error_handler.js
@@ -3,7 +3,6 @@
 const { serializeError } = require('serialize-error')
 
 const logger = require('./logger')
-
 const httpCode = require('./http_code')
 
 const errorHandler = (err, res = null) => {
@@ -12,16 +11,15 @@ const errorHandler = (err, res = null) => {
   const errorTrace = err.errorTrace || serializeError(err) || null
 
   if (res && res.sendJsonAndLog) {
-    return res.sendJsonAndLog(false, message, statusCode)
+    return res.sendJsonAndLog(false, message, statusCode, null, errorTrace)
   }
 
   logger.error(message, {
-    information: errorTrace,
-    statusCode: statusCode,
+    error: errorTrace,
+    status: statusCode,
     success: false
   })
   // exit node with error
   process.exit(1)
 }
-
 module.exports = errorHandler
