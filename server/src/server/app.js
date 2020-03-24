f2146f3d6e322a6ba209382b480842cb367f9da7
Felipe Diniz <diniz.ime@gmail.com>
2020-03-24 20:23:24 -0300

    fix packages

--- server/src/server/app.js
+++ server/src/server/app.js
@@ -1,18 +1,19 @@
-'use strict'
+"use strict";
 
-const express = require('express')
-const path = require('path')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const helmet = require('helmet')
-const xss = require('xss-clean')
-const hpp = require('hpp')
-const rateLimit = require('express-rate-limit')
-const swaggerUi = require('swagger-ui-express')
-const swaggerJSDoc = require('swagger-jsdoc')
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const rateLimit = require("express-rate-limit");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJSDoc = require("swagger-jsdoc");
+const noCache = require("nocache");
 
-const swaggerOptions = require('./swagger_options')
-const swaggerSpec = swaggerJSDoc(swaggerOptions)
+const swaggerOptions = require("./swagger_options");
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
 const {
   AppError,
@@ -20,69 +21,72 @@ const {
   logger,
   errorHandler,
   sendJsonAndLogMiddleware
-} = require('../utils')
+} = require("../utils");
 
-const appRoutes = require('../routes')
+const appRoutes = require("../routes");
 
-const app = express()
+const app = express();
 
 // Add sendJsonAndLog to res object
-app.use(sendJsonAndLogMiddleware)
+app.use(sendJsonAndLogMiddleware);
 
-app.use(bodyParser.json()) // parsear POST em JSON
-app.use(hpp()) // protection against parameter polution
-app.use(xss())
+app.use(bodyParser.json()); // parsear POST em JSON
+app.use(hpp()); // protection against parameter polution
+app.use(xss());
 
 // CORS middleware
-app.use(cors())
+app.use(cors());
 
 // Helmet Protection
-app.use(helmet())
+app.use(helmet());
 // Disables cache https://helmetjs.github.io/docs/nocache/
-app.use(helmet.noCache())
+app.use(noCache());
 
 const limiter = rateLimit({
   windowMs: 60 * 1000, // 1 minuto
   max: 200 // limite por minuto por IP
-})
+});
 
 // apply limit all requests
-app.use(limiter)
+app.use(limiter);
 
 app.use((req, res, next) => {
-  const url = req.protocol + '://' + req.get('host') + req.originalUrl
+  const url = req.protocol + "://" + req.get("host") + req.originalUrl;
 
   logger.info(`${req.method} request`, {
     url,
     ip: req.ip
-  })
-  return next()
-})
+  });
+  return next();
+});
 
 // All routes used by the App
-app.use('/api', appRoutes)
+app.use("/api", appRoutes);
 
 // Serve SwaggerDoc
-app.use('/api/api_docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+app.use("/api/api_docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Serve JSDocs
-app.use('/api/js_docs', express.static(path.join(__dirname, '..', 'js_docs')))
+app.use("/api/js_docs", express.static(path.join(__dirname, "..", "js_docs")));
 
 // Serve Client
-app.use(express.static(path.join(__dirname, '..', 'build')))
+app.use(express.static(path.join(__dirname, "..", "build")));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
-})
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
 
 app.use((req, res, next) => {
-  const err = new AppError(`URL não encontrada para o método ${req.method}`, httpCode.NotFound)
-  return next(err)
-})
+  const err = new AppError(
+    `URL não encontrada para o método ${req.method}`,
+    httpCode.NotFound
+  );
+  return next(err);
+});
 
 // Error handling
 app.use((err, req, res, next) => {
-  return errorHandler.log(err, res)
-})
+  return errorHandler.log(err, res);
+});
 
-module.exports = app
+module.exports = app;
