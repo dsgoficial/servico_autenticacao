97c5e01aa08d91b4cd7d2977a5d1b5d9d9dc89b7
Felipe Diniz <diniz.ime@gmail.com>
2020-08-09 20:48:19 -0300

    problem with mateiral table

--- client/src/GerenciaUsuarios/dialogo_adiciona.js
+++ client/src/GerenciaUsuarios/dialogo_adiciona.js
@@ -1,71 +1,75 @@
-import React, { useState, useEffect } from 'react'
-import Button from '@material-ui/core/Button'
-import Dialog from '@material-ui/core/Dialog'
-import DialogActions from '@material-ui/core/DialogActions'
-import DialogContent from '@material-ui/core/DialogContent'
-import DialogContentText from '@material-ui/core/DialogContentText'
-import DialogTitle from '@material-ui/core/DialogTitle'
-import ReactLoading from 'react-loading'
-import { Formik, Form, Field } from 'formik'
-import { TextField, Select, CheckboxWithLabel } from 'formik-material-ui'
-import MenuItem from '@material-ui/core/MenuItem'
+import React, { useState, useEffect } from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import ReactLoading from "react-loading";
+import { Formik, Form, Field } from "formik";
+import { TextField, Select, CheckboxWithLabel } from "formik-material-ui";
+import MenuItem from "@material-ui/core/MenuItem";
 
-import { criaUsuario, getSelectData } from './api'
-import { usuarioSchema } from './validation_schema'
-import { SubmitButton } from '../helpers'
-import styles from './styles'
+import { criaUsuario, getSelectData } from "./api";
+import { usuarioSchema } from "./validation_schema";
+import { SubmitButton } from "../helpers";
+import styles from "./styles";
 
 const DialogoAdiciona = ({ open = false, handleDialog }) => {
-  const classes = styles()
+  const classes = styles();
 
   const initialValues = {
-    uuid: '',
-    usuario: '',
-    nome: '',
-    nomeGuerra: '',
-    tipoPostoGradId: '',
-    tipoTurnoId: '',
+    uuid: "",
+    usuario: "",
+    nome: "",
+    nomeGuerra: "",
+    tipoPostoGradId: "",
+    tipoTurnoId: "",
     ativo: false,
-    administrador: false
-  }
+    administrador: false,
+  };
 
-  const [listaPostoGrad, setListaPostoGrad] = useState([])
-  const [listaTurno, setListaTurno] = useState([])
+  const [listaPostoGrad, setListaPostoGrad] = useState([]);
+  const [listaTurno, setListaTurno] = useState([]);
 
-  const [submitting, setSubmitting] = useState(false)
-  const [loaded, setLoaded] = useState(false)
+  const [submitting, setSubmitting] = useState(false);
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    let isCurrent = true
+    let isCurrent = true;
 
     const load = async () => {
       try {
-        const response = await getSelectData()
-        if (!response || !isCurrent) return
+        const response = await getSelectData();
+        if (!response || !isCurrent) return;
 
-        setListaPostoGrad(response.listaPostoGrad)
-        setListaTurno(response.listaTurno)
-        setLoaded(true)
+        setListaPostoGrad(response.listaPostoGrad);
+        setListaTurno(response.listaTurno);
+        setLoaded(true);
       } catch (err) {
-        handleDialog && handleDialog('error', 'Ocorreu um erro ao se comunicar com o servidor.')
+        handleDialog &&
+          handleDialog(
+            "error",
+            "Ocorreu um erro ao se comunicar com o servidor."
+          );
       }
-    }
-    load()
+    };
+    load();
     return () => {
-      isCurrent = false
-    }
-  }, [handleDialog])
+      isCurrent = false;
+    };
+  }, [handleDialog]);
 
   const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return
+    if (reason === "clickaway") {
+      return;
     }
-    handleDialog && handleDialog()
-  }
+    handleDialog && handleDialog();
+  };
 
   const handleForm = async (values, { resetForm }) => {
     try {
-      setSubmitting(true)
+      setSubmitting(true);
       const response = await criaUsuario(
         values.usuario,
         values.nome,
@@ -75,24 +79,28 @@ const DialogoAdiciona = ({ open = false, handleDialog }) => {
         values.ativo,
         values.administrador,
         values.uuid
-      )
-      if (!response) return
-      setSubmitting(false)
-      handleDialog && handleDialog('success', 'Usuário criado com sucesso.')
+      );
+      if (!response) return;
+      setSubmitting(false);
+      handleDialog && handleDialog("success", "Usuário criado com sucesso.");
     } catch (err) {
-      setSubmitting(false)
-      resetForm(initialValues)
+      setSubmitting(false);
+      resetForm(initialValues);
       if (
-        'response' in err &&
-        'data' in err.response &&
-        'message' in err.response.data
+        "response" in err &&
+        "data" in err.response &&
+        "message" in err.response.data
       ) {
-        handleDialog && handleDialog('error', err.response.data.message)
+        handleDialog && handleDialog("error", err.response.data.message);
       } else {
-        handleDialog && handleDialog('error', 'Ocorreu um erro ao se comunicar com o servidor.')
+        handleDialog &&
+          handleDialog(
+            "error",
+            "Ocorreu um erro ao se comunicar com o servidor."
+          );
       }
     }
-  }
+  };
 
   return (
     <Dialog open={open} onClose={handleClose}>
@@ -108,42 +116,42 @@ const DialogoAdiciona = ({ open = false, handleDialog }) => {
               {({ isValid, isSubmitting, isValidating }) => (
                 <Form className={classes.form}>
                   <Field
-                    name='usuario'
+                    name="usuario"
                     component={TextField}
-                    variant='outlined'
-                    margin='normal'
+                    variant="outlined"
+                    margin="normal"
                     fullWidth
-                    label='Usuário'
+                    label="Usuário"
                   />
                   <Field
-                    name='nome'
+                    name="nome"
                     component={TextField}
-                    variant='outlined'
-                    margin='normal'
+                    variant="outlined"
+                    margin="normal"
                     fullWidth
-                    label='Nome completo'
+                    label="Nome completo"
                   />
                   <Field
-                    name='nomeGuerra'
+                    name="nomeGuerra"
                     component={TextField}
-                    variant='outlined'
-                    margin='normal'
+                    variant="outlined"
+                    margin="normal"
                     fullWidth
-                    label='Nome de guerra'
+                    label="Nome de guerra"
                   />
                   <div>
                     <Field
-                      name='tipoPostoGradId'
-                      label='Posto/Graduação'
-                      variant='outlined'
+                      name="tipoPostoGradId"
+                      label="Posto/Graduação"
+                      variant="outlined"
                       component={Select}
                       displayEmpty
                       className={classes.select}
                     >
-                      <MenuItem value='' disabled>
+                      <MenuItem value="" disabled>
                         Selecione seu Posto/Graduação
                       </MenuItem>
-                      {listaPostoGrad.map(option => (
+                      {listaPostoGrad.map((option) => (
                         <MenuItem key={option.code} value={option.code}>
                           {option.nome}
                         </MenuItem>
@@ -152,17 +160,17 @@ const DialogoAdiciona = ({ open = false, handleDialog }) => {
                   </div>
                   <div>
                     <Field
-                      name='tipoTurnoId'
-                      label='Turno'
-                      variant='outlined'
+                      name="tipoTurnoId"
+                      label="Turno"
+                      variant="outlined"
                       component={Select}
                       displayEmpty
                       className={classes.select}
                     >
-                      <MenuItem value='' disabled>
+                      <MenuItem value="" disabled>
                         Selecione seu turno de trabalho
                       </MenuItem>
-                      {listaTurno.map(option => (
+                      {listaTurno.map((option) => (
                         <MenuItem key={option.code} value={option.code}>
                           {option.nome}
                         </MenuItem>
@@ -171,37 +179,41 @@ const DialogoAdiciona = ({ open = false, handleDialog }) => {
                   </div>
                   <div>
                     <Field
-                      name='administrador'
+                      name="administrador"
+                      type="checkbox"
                       component={CheckboxWithLabel}
-                      variant='outlined'
-                      margin='normal'
-                      Label={{ label: 'Administrador' }}
-                      color='primary'
+                      variant="outlined"
+                      margin="normal"
+                      Label={{ label: "Administrador" }}
+                      color="primary"
                     />
                   </div>
                   <div>
                     <Field
-                      name='ativo'
+                      name="ativo"
+                      type="checkbox"
                       component={CheckboxWithLabel}
-                      variant='outlined'
-                      margin='normal'
-                      Label={{ label: 'Ativo' }}
-                      color='primary'
+                      variant="outlined"
+                      margin="normal"
+                      Label={{ label: "Ativo" }}
+                      color="primary"
                     />
                   </div>
                   <Field
-                    name='uuid'
+                    name="uuid"
                     component={TextField}
-                    variant='outlined'
-                    margin='normal'
+                    variant="outlined"
+                    margin="normal"
                     fullWidth
-                    label='UUID'
+                    label="UUID"
                   />
                   <SubmitButton
-                    type='submit' disabled={isValidating || !isValid} submitting={isSubmitting}
+                    type="submit"
+                    disabled={isValidating || !isValid}
+                    submitting={isSubmitting}
                     fullWidth
-                    variant='contained'
-                    color='primary'
+                    variant="contained"
+                    color="primary"
                     className={classes.submit}
                   >
                     Cadastrar
@@ -216,20 +228,29 @@ const DialogoAdiciona = ({ open = false, handleDialog }) => {
               A senha do usuário criado será igual ao login.
             </DialogContentText>
           </>
-        )
-          : (
-            <div className={classes.loading}>
-              <ReactLoading type='bars' color='#F83737' height='40%' width='40%' />
-            </div>
-          )}
+        ) : (
+          <div className={classes.loading}>
+            <ReactLoading
+              type="bars"
+              color="#F83737"
+              height="40%"
+              width="40%"
+            />
+          </div>
+        )}
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleClose} color='primary' disabled={submitting} autoFocus>
+        <Button
+          onClick={handleClose}
+          color="primary"
+          disabled={submitting}
+          autoFocus
+        >
           Cancelar
         </Button>
       </DialogActions>
     </Dialog>
-  )
-}
+  );
+};
 
-export default DialogoAdiciona
+export default DialogoAdiciona;
--- server/src/dashboard/dashboard_ctrl.js
+++ server/src/dashboard/dashboard_ctrl.js
@@ -1,39 +1,40 @@
-'use strict'
+"use strict";
 
-const { db } = require('../database')
+const { db } = require("../database");
 
-const controller = {}
+const controller = {};
 
 controller.getUsuariosLogados = async () => {
   return db.conn.any(
     `SELECT ROW_NUMBER () OVER (ORDER BY l.ultimo_login DESC) AS id,
-    l.ultimo_login, u.login, u.nome_guerra, tpg.nome_abrev AS tipo_posto_grad, a.nome_abrev AS aplicacao
+    l.ultimo_login, u.login, u.nome_guerra, tpg.nome_abrev AS tipo_posto_grad, tt.nome AS tipo_turno, a.nome_abrev AS aplicacao
     FROM dgeo.usuario AS u
     INNER JOIN dominio.tipo_posto_grad AS tpg ON tpg.code = u.tipo_posto_grad_id
-    INNER JOIN
+    INNER JOIN dominio.tipo_turno AS tt ON tt.code = u.tipo_turno_id
+    INNER JOIN 
     (SELECT aplicacao_id, usuario_id, max(data_login) AS ultimo_login FROM dgeo.login GROUP BY usuario_id, aplicacao_id) AS l
     ON l.usuario_id = u.id
     INNER JOIN dgeo.aplicacao AS a ON a.id = l.aplicacao_id
     WHERE l.ultimo_login::date = now()::date
     ORDER BY l.ultimo_login DESC`
-  )
-}
+  );
+};
 
 controller.getUsuariosAtivos = async () => {
   const result = await db.conn.one(
-    'SELECT count(*) FROM dgeo.usuario WHERE ativo IS TRUE'
-  )
+    "SELECT count(*) FROM dgeo.usuario WHERE ativo IS TRUE"
+  );
 
-  return result.count
-}
+  return result.count;
+};
 
 controller.getAplicacoesAtivas = async () => {
   const result = await db.conn.one(
-    'SELECT count(*) FROM dgeo.aplicacao WHERE ativa IS TRUE'
-  )
+    "SELECT count(*) FROM dgeo.aplicacao WHERE ativa IS TRUE"
+  );
 
-  return result.count
-}
+  return result.count;
+};
 
 controller.getLoginsDia = async (total = 14) => {
   const result = await db.conn.any(
@@ -44,13 +45,13 @@ controller.getLoginsDia = async (total = 14) => {
     ORDER BY day::date
     `,
     { total: total - 1 }
-  )
-  result.forEach(r => {
-    r.logins = +r.logins
-  })
+  );
+  result.forEach((r) => {
+    r.logins = +r.logins;
+  });
 
-  return result
-}
+  return result;
+};
 
 controller.getLoginsMes = async (total = 12) => {
   const result = await db.conn.any(
@@ -61,14 +62,14 @@ controller.getLoginsMes = async (total = 12) => {
     ORDER BY date_trunc('month', month.month)
     `,
     { total: total - 1 }
-  )
+  );
 
-  result.forEach(r => {
-    r.logins = +r.logins
-  })
+  result.forEach((r) => {
+    r.logins = +r.logins;
+  });
 
-  return result
-}
+  return result;
+};
 
 controller.getLoginsAplicacoes = async (total = 14, max = 10) => {
   const aplicacoes = await db.conn.any(
@@ -80,7 +81,7 @@ controller.getLoginsAplicacoes = async (total = 14, max = 10) => {
     ORDER BY count(l.id) DESC
     LIMIT $<max:raw>`,
     { total: total - 1, max: max - 1 }
-  )
+  );
   const dados = await db.conn.any(
     `SELECT day::date AS data_login,  COALESCE(a.nome_abrev, 'Aplicação deletada') AS aplicacao, count(l.id) AS logins FROM 
     generate_series((now() - interval '$<total:raw> day')::date, now()::date, interval  '1 day') AS day
@@ -89,35 +90,35 @@ controller.getLoginsAplicacoes = async (total = 14, max = 10) => {
     GROUP BY day::date, a.nome_abrev
     ORDER BY day::date, a.nome_abrev`,
     { total: total - 1 }
-  )
+  );
 
-  const aplicacoesFixed = []
-  aplicacoes.forEach(a => aplicacoesFixed.push(a.aplicacao))
+  const aplicacoesFixed = [];
+  aplicacoes.forEach((a) => aplicacoesFixed.push(a.aplicacao));
 
-  const resultDict = {}
-  dados.forEach(d => {
+  const resultDict = {};
+  dados.forEach((d) => {
     if (!(d.data_login in resultDict)) {
-      resultDict[d.data_login] = {}
-      resultDict[d.data_login].data = d.data_login.toISOString().split('T')[0]
-      aplicacoesFixed.forEach(a => {
-        resultDict[d.data_login][a] = 0
-      })
-      resultDict[d.data_login].outros = 0
+      resultDict[d.data_login] = {};
+      resultDict[d.data_login].data = d.data_login.toISOString().split("T")[0];
+      aplicacoesFixed.forEach((a) => {
+        resultDict[d.data_login][a] = 0;
+      });
+      resultDict[d.data_login].outros = 0;
     }
     if (aplicacoesFixed.indexOf(d.aplicacao) !== -1) {
-      resultDict[d.data_login][d.aplicacao] = +d.logins
+      resultDict[d.data_login][d.aplicacao] = +d.logins;
     } else {
-      resultDict[d.data_login].outros += d.logins
+      resultDict[d.data_login].outros += d.logins;
     }
-  })
+  });
 
-  const result = []
-  Object.keys(resultDict).forEach(key => {
-    result.push(resultDict[key])
-  })
+  const result = [];
+  Object.keys(resultDict).forEach((key) => {
+    result.push(resultDict[key]);
+  });
 
-  return result
-}
+  return result;
+};
 
 controller.getLoginsUsuarios = async (total = 14, max = 10) => {
   const usuarios = await db.conn.any(
@@ -130,7 +131,7 @@ controller.getLoginsUsuarios = async (total = 14, max = 10) => {
     ORDER BY count(l.id) DESC
     LIMIT $<max:raw>`,
     { total: total - 1, max: max - 1 }
-  )
+  );
   const dados = await db.conn.any(
     `SELECT day::date AS data_login,  COALESCE(pg.nome_abrev || ' ' || u.nome_guerra, 'Usuário deletado') AS usuario, count(l.id) AS logins FROM 
     generate_series((now() - interval '$<total:raw> day')::date, now()::date, interval  '1 day') AS day
@@ -140,34 +141,34 @@ controller.getLoginsUsuarios = async (total = 14, max = 10) => {
     GROUP BY day::date, pg.nome_abrev || ' ' || u.nome_guerra
     ORDER BY day::date, pg.nome_abrev || ' ' || u.nome_guerra`,
     { total: total - 1 }
-  )
+  );
 
-  const usuariosFixed = []
-  usuarios.forEach(a => usuariosFixed.push(a.usuario))
+  const usuariosFixed = [];
+  usuarios.forEach((a) => usuariosFixed.push(a.usuario));
 
-  const resultDict = {}
-  dados.forEach(d => {
+  const resultDict = {};
+  dados.forEach((d) => {
     if (!(d.data_login in resultDict)) {
-      resultDict[d.data_login] = {}
-      resultDict[d.data_login].data = d.data_login.toISOString().split('T')[0]
-      usuariosFixed.forEach(u => {
-        resultDict[d.data_login][u] = 0
-      })
-      resultDict[d.data_login].outros = 0
+      resultDict[d.data_login] = {};
+      resultDict[d.data_login].data = d.data_login.toISOString().split("T")[0];
+      usuariosFixed.forEach((u) => {
+        resultDict[d.data_login][u] = 0;
+      });
+      resultDict[d.data_login].outros = 0;
     }
     if (usuariosFixed.indexOf(d.usuario) !== -1) {
-      resultDict[d.data_login][d.usuario] = +d.logins
+      resultDict[d.data_login][d.usuario] = +d.logins;
     } else {
-      resultDict[d.data_login].outros += d.logins
+      resultDict[d.data_login].outros += d.logins;
     }
-  })
+  });
 
-  const result = []
-  Object.keys(resultDict).forEach(key => {
-    result.push(resultDict[key])
-  })
+  const result = [];
+  Object.keys(resultDict).forEach((key) => {
+    result.push(resultDict[key]);
+  });
 
-  return result
-}
+  return result;
+};
 
-module.exports = controller
+module.exports = controller;
