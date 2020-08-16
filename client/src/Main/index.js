a8e167d7ef197da3f284c1efc2baca2d7e898b1f
Felipe Diniz <diniz.ime@gmail.com>
2020-08-16 19:33:57 -0300

    fix rout

--- client/src/Main/index.js
+++ client/src/Main/index.js
@@ -1,64 +1,81 @@
-import React, { useState } from 'react'
-import { withRouter, HashRouter, Route } from 'react-router-dom'
-import clsx from 'clsx'
-import AppBar from '@material-ui/core/AppBar'
-import Drawer from '@material-ui/core/Drawer'
-import Container from '@material-ui/core/Container'
-import Toolbar from '@material-ui/core/Toolbar'
-import IconButton from '@material-ui/core/IconButton'
-import Typography from '@material-ui/core/Typography'
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
-import MenuIcon from '@material-ui/icons/Menu'
-import ExitToAppIcon from '@material-ui/icons/ExitToApp'
+import React, { useState } from "react";
+import { withRouter, HashRouter } from "react-router-dom";
+import clsx from "clsx";
+import AppBar from "@material-ui/core/AppBar";
+import Drawer from "@material-ui/core/Drawer";
+import Container from "@material-ui/core/Container";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import MenuIcon from "@material-ui/icons/Menu";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
-import styles from './styles'
-import { MainListItems, AdminListItems } from './list_items'
-import { handleLogout } from './api.js'
-import { PrivateRoute } from '../helpers'
+import styles from "./styles";
+import { MainListItems, AdminListItems } from "./list_items";
+import { handleLogout } from "./api.js";
+import { PrivateRoute } from "../helpers";
 
-import InformacaoUsuario from '../InformacaoUsuario'
-import AlteraSenha from '../AlteraSenha'
-import GerenciaUsuarios from '../GerenciaUsuarios'
-import AutorizaUsuarios from '../AutorizaUsuarios'
-import GerenciarAplicacoes from '../GerenciarAplicacoes'
-import Dashboard from '../Dashboard'
+import InformacaoUsuario from "../InformacaoUsuario";
+import AlteraSenha from "../AlteraSenha";
+import GerenciaUsuarios from "../GerenciaUsuarios";
+import AutorizaUsuarios from "../AutorizaUsuarios";
+import GerenciarAplicacoes from "../GerenciarAplicacoes";
+import Dashboard from "../Dashboard";
 
-export default withRouter(props => {
-  const classes = styles()
+export default withRouter((props) => {
+  const classes = styles();
 
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
   const handleDrawerOpen = () => {
-    setOpen(true)
-  }
+    setOpen(true);
+  };
   const handleDrawerClose = () => {
-    setOpen(false)
-  }
+    setOpen(false);
+  };
 
   const clickLogout = () => {
-    handleLogout()
-    props.history.push('/login')
-  }
+    handleLogout();
+    props.history.push("/login");
+  };
 
   return (
     <div className={classes.root}>
       <HashRouter>
-        <AppBar position='absolute' className={clsx(classes.appBar, open && classes.appBarShift)}>
+        <AppBar
+          position="absolute"
+          className={clsx(classes.appBar, open && classes.appBarShift)}
+        >
           <Toolbar className={classes.toolbar}>
             <IconButton
-              edge='start'
-              color='inherit'
-              aria-label='open drawer'
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
               onClick={handleDrawerOpen}
-              className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+              className={clsx(
+                classes.menuButton,
+                open && classes.menuButtonHidden
+              )}
             >
               <MenuIcon />
             </IconButton>
-            <Typography component='h1' variant='h6' color='inherit' noWrap className={classes.title}>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              className={classes.title}
+            >
               Serviço de Autenticação
             </Typography>
-            <IconButton color='inherit' onClick={clickLogout}>
-              <Typography variant='body1' color='inherit' noWrap className={classes.title}>
+            <IconButton color="inherit" onClick={clickLogout}>
+              <Typography
+                variant="body1"
+                color="inherit"
+                noWrap
+                className={classes.title}
+              >
                 Sair
               </Typography>
               <ExitToAppIcon className={classes.logoutButton} />
@@ -66,14 +83,14 @@ export default withRouter(props => {
           </Toolbar>
         </AppBar>
         <Drawer
-          variant='permanent'
+          variant="permanent"
           classes={{
-            paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose)
+            paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
           }}
           open={open}
         >
           <div className={classes.toolbarIcon}>
-            <Typography variant='h6' className={classes.menu}>
+            <Typography variant="h6" className={classes.menu}>
               Menu
             </Typography>
             <IconButton onClick={handleDrawerClose}>
@@ -81,23 +98,44 @@ export default withRouter(props => {
             </IconButton>
           </div>
           <MainListItems />
-          {props.role === 'ADMIN' &&
+          {props.role === "ADMIN" && (
             <>
               <AdminListItems />
-            </>}
+            </>
+          )}
         </Drawer>
         <main className={classes.content}>
           <div className={classes.appBarSpacer} />
-          <Container maxWidth='xl' className={classes.container}>
-            <Route exact path='/' component={InformacaoUsuario} />
-            <Route exact path='/alterar_senha' component={AlteraSenha} />
-            <PrivateRoute role='ADMIN' exact path='/gerenciar_usuarios' component={GerenciaUsuarios} />
-            <PrivateRoute role='ADMIN' exact path='/autorizar_usuarios' component={AutorizaUsuarios} />
-            <PrivateRoute role='ADMIN' exact path='/dashboard' component={Dashboard} />
-            <PrivateRoute role='ADMIN' exact path='/gerenciar_aplicacoes' component={GerenciarAplicacoes} />
+          <Container maxWidth="xl" className={classes.container}>
+            <PrivateRoute exact path="/" component={InformacaoUsuario} />
+            <PrivateRoute exact path="/alterar_senha" component={AlteraSenha} />
+            <PrivateRoute
+              role="ADMIN"
+              exact
+              path="/gerenciar_usuarios"
+              component={GerenciaUsuarios}
+            />
+            <PrivateRoute
+              role="ADMIN"
+              exact
+              path="/autorizar_usuarios"
+              component={AutorizaUsuarios}
+            />
+            <PrivateRoute
+              role="ADMIN"
+              exact
+              path="/dashboard"
+              component={Dashboard}
+            />
+            <PrivateRoute
+              role="ADMIN"
+              exact
+              path="/gerenciar_aplicacoes"
+              component={GerenciarAplicacoes}
+            />
           </Container>
         </main>
       </HashRouter>
     </div>
-  )
-})
+  );
+});
